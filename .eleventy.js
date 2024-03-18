2c2eaecd7ab62566270ac579afc58cce17d1efa5
Saqib Hussain <saqib@aganitha.ai>
2024-03-18 15:39:35 +0530

    basic template created

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addWatchTarget("./src/css");
+
+  eleventyConfig.addFilter("blogDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
